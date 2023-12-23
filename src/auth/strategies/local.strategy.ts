16c9e55a5788f39ec6a48e6a4d8d41f08242ee82
Mateus Mendonça <mateus.mlo95@gmail.com>
2023-12-22 22:35:50 -0300

    add local auth strategy

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+  private logger = new Logger('LocalStrategy');
+
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password);
+
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
+}
